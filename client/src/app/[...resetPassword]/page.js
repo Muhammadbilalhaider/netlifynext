f21fb5ab10725be83b3f8000da23d65bdbf5ab8d
zaman <mzamanmehmood95@gmail.com>
2025-02-15 00:53:50 +0500

    login

---
+++ client/src/app/[...resetPassword]/page.js
@@ -0,0 +1,79 @@
+'use client'
+import { KeyRound } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+
+const page = () => {
+  const navigate = useRouter()
+    const [formData, setFormData] = useState({
+    email: '', password: '', linkHash: null, newPassword: '', confirmPassword: ''
+  });
+
+
+  
+  const handleChaneForm = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+
+  const handleResetPassword = async () => {
+    const response = await axios.post('http://localhost:8087/api_v1/auth/reset-password',
+      {
+        _id: formData._id,
+        linkHash: formData.linkHash,
+        password: formData.password,
+        confirmPassword: formData.confirmPassword
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    if (response.data == "200") {
+     navigate.push('/login')
+    }
+
+  }
+
+
+
+  return (
+    <div className='flex w-full flex-col justify-center items-center space-y-4'>
+         <div className='flex flex-col w-full lg:w-2/6 h-full shadow-lg bg-white justify-center items-center space-y-4 my-20 py-8 px-6 rounded-2xl'>
+            <span className='text-[#217346]'><KeyRound size={60} /></span>
+
+            <p className='text-xl font-bold text-center'>Create New Password</p>
+            <div className='flex flex-col w-full space-y-3'>
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='text'
+                name='linkHash'
+                value={formData.linkHash}
+                onChange={handleChaneForm}
+                placeholder='Reset Code' />
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='password'
+               name='newPassword'
+                value={formData.newPassword}
+                onChange={handleChaneForm}
+                placeholder='New Password' />
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='password'
+                name='confirmPassword' 
+                value={formData.confirmPassword}
+                onChange={handleChaneForm}
+                placeholder='Confirm Password' />
+
+            </div>
+
+            <div className='flex flex-col w-full space-y-3'>
+              <button className='w-full bg-[#217346] text-white p-2 rounded-lg font-semibold' onClick={handleResetPassword}>Reset Password</button>
+
+            </div>
+          </div>
+
+    </div>
+  )
+}
+
+export default page
--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { KeyRound } from 'lucide-react'
 import { useRouter } from 'next/navigation';
 
@@ -9,9 +9,10 @@ const Login = () => {
   const [openLogin, setOpenLogin] = useState(true);
   const [resetPass, setOpenResetPass] = useState(false);
   const [formData, setFormData] = useState({
-    email: '', password: '',
+    email: '', password: '', linkHash: null, newPassword: '', confirmPassword: ''
   });
 
+  const userID = localStorage.getItem("userId")
   const navigate = useRouter();
   const openForgotPass = () => {
     setOpenLogin(false);
@@ -27,22 +28,9 @@ const Login = () => {
   }
 
 
-  const handleResetCode = () => {
-    setOpenForgotpass(false);
-    setOpenLogin(false);
-    setOpenResetPass(true);
-  }
-
-  const handleReestPass = () => {
-    setOpenForgotpass(false);
-
-    setOpenResetPass(false);
-    setOpenLogin(true);
-  }
-
   const handleChaneForm = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
-}
+  }
 
   const handleUserSignIn = async () => {
     try {
@@ -57,12 +45,58 @@ const Login = () => {
           },
         })
       console.log("User SignedIn: ", response.data)
-      localStorage.setItem('user',response.data)
+      const userId = response.data?.data?._id;
+      localStorage.setItem('user', JSON.stringify(response.data));
+      localStorage.setItem('userId', userId);
     } catch (error) {
       console.log("Error registering user:", error.response?.data || error.message);
 
     }
   }
+
+
+  const handleSendResetCode = async () => {
+    const response = await axios.post('http://localhost:8087/api_v1/auth/forgot-password',
+      {
+        email: formData.email
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    if (response.data.status_code == '200') {
+      setOpenForgotpass(false);
+      setOpenLogin(false);
+      setOpenResetPass(true);
+    }
+  }
+
+  useEffect(()=>{},[userID])
+
+  const handleResetPassword = async () => {
+    const response = await axios.post('http://localhost:8087/api_v1/auth/reset-password',
+      {
+        _id: userID,
+        linkHash: parseInt(formData.linkHash),
+        password: formData.newPassword,
+       
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    if (response.data == "200") {
+      setOpenForgotpass(false);
+      setOpenResetPass(false);
+      setOpenLogin(true);
+    }
+
+  }
+
 
   return (
     <div className='flex w-full flex-col justify-center items-center space-y-4'>
@@ -96,27 +130,28 @@ const Login = () => {
         </div>
       )}
 
-
-
-
-
       {forgotPass && (
         <div className='flex flex-col w-full lg:w-2/6 h-full shadow-lg bg-white justify-center items-center space-y-4 my-20 py-8 px-6 rounded-2xl'>
           <span className='text-[#217346]'><KeyRound size={60} /></span>
 
           <p className='text-xl font-bold text-center'>Reset password</p>
           <div className='flex flex-col w-full space-y-3'>
-            <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg' type='email' placeholder='Email' />
+            <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+              type='email'
+              value={formData.email}
+              name='email'
+              onChange={handleChaneForm}
+              placeholder='Email' />
           </div>
           <div className='flex flex-col w-full space-y-3'>
-            <button className='w-full bg-[#217346] text-white p-2 rounded-lg font-semibold' onClick={handleResetCode}>Send Reset Code</button>
+            <button className='w-full bg-[#217346] text-white p-2 rounded-lg font-semibold'
+
+              onClick={handleSendResetCode}>Send Reset Code</button>
             <button className='w-full bg-transparent text-[#217346] p-2 border-2 border-[#217346] rounded-lg font-semibold' onClick={handleBackLogin}>Back to Login</button>
 
           </div>
         </div>
       )}
-
-
 
       {
         resetPass && (
@@ -125,14 +160,29 @@ const Login = () => {
 
             <p className='text-xl font-bold text-center'>Create New Password</p>
             <div className='flex flex-col w-full space-y-3'>
-              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg' type='email' placeholder='Reset Code' />
-              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg' type='password' placeholder='New Password' />
-              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg' type='password' placeholder='Confirm Password' />
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='text'
+                name='linkHash'
+                value={formData.linkHash}
+                onChange={handleChaneForm}
+                placeholder='Reset Code' />
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='password'
+               name='newPassword'
+                value={formData.newPassword}
+                onChange={handleChaneForm}
+                placeholder='New Password' />
+              <input className='p-3 w-full border-2 border-[#247BAF]  focus:outline-none focus:ring-1 focus:ring-[#247BAF] rounded-lg'
+                type='password'
+                name='confirmPassword' 
+                value={formData.confirmPassword}
+                onChange={handleChaneForm}
+                placeholder='Confirm Password' />
 
             </div>
 
             <div className='flex flex-col w-full space-y-3'>
-              <button className='w-full bg-[#217346] text-white p-2 rounded-lg font-semibold' onClick={handleReestPass}>Reset Password</button>
+              <button className='w-full bg-[#217346] text-white p-2 rounded-lg font-semibold' onClick={handleResetPassword}>Reset Password</button>
 
             </div>
           </div>
